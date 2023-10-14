6278f5792711f508ea512d51e4e2982f41a85bbb
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-10-14 21:19:45 +0300

    add icons & useState for likes

--- src/app/LikeMyPhoto.jsx
+++ src/app/LikeMyPhoto.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../components/Title";
-import {AiFillSmile,AiOutlineHeart,AiOutlineComment} from "react-icons/ai"
+import {AiFillSmile,AiFillHeart,AiOutlineHeart,AiOutlineComment} from "react-icons/ai"
+import girl from "../girl.jpg"
 export default function RandomizeColors() {
+  const [like, setlike] = useState(false)
+  const [count, setcount] = useState(0)
+  const toggleLike=()=>{
+    if(!like){
+      setlike(true)
+      setcount(count + 1);
+    }else{
+      setlike(false)
+      setcount(count - 1);
 
+    }
+  }
   return (
-    <main className="container m-top-2">
+    <main className="container m-top-2 " style={{textAlign:"center"}}>
       <h1>
 
         <Title
@@ -13,15 +25,15 @@ export default function RandomizeColors() {
         />
         <Title
           classes={"mb-4 subtitle text-center"}
-          title={" Likes: "}
+          title={`LIKE:${count}`}
         />
 
-        <div className="card" style={{width:"300px"}}>
+        <div className="card" style={{width:"300px",cursor:"pointer"}}>
           <p className="text-center text-white" style={{ color:"white" }}> <AiFillSmile className="m-right-2"/> MY PHOTO</p>
-          <img src="girl.jpg" alt="girl"  style={{ height:"fit-content" }} />
+          <img src={girl} alt="girl"    width={300}/>
           <div className="d-flex" style={{ justifyContent:"space-between" }}>
-            <AiOutlineComment style={{ color:"white" }}/>
-            <AiOutlineHeart style={{ color:"white" }}/>
+            <AiOutlineComment style={{ color:"white" ,padding:"1rem"}} />
+            {like ? (<AiFillHeart onClick={toggleLike} style={{ color:"red" ,padding:"1rem"}}/>):(<AiOutlineHeart  onClick={toggleLike} style={{ color:"white" ,padding:"1rem"}}/>)}
           </div>
         </div>
       </h1>
